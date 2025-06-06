d361931bc1f5d0384b4822661d935789bc280e9c
Sil0105 <pipita0105@gmail.com>
2025-06-06 20:05:55 -0300

    Agrego codigo a ejercicio 8

---
+++ Clase_9/ejercicio8.js
@@ -0,0 +1,21 @@
+//Pide al usuario que ingrese las longitudes de los tres lados de un triángulo. 
+// Determina y muestra si el triángulo es equilátero, isósceles o escaleno. 
+// (Investiga sobre los triángulos para determinar la formula)
+
+const prompt = require('prompt-sync')();
+
+
+let lado1 = parseFloat(prompt("Ingrese la longitud del primer lado: "));
+let lado2 = parseFloat(prompt("Ingrese la longitud del segundo lado: "));
+let lado3 = parseFloat(prompt("Ingrese la longitud del tercer lado: "));
+
+if (lado1 === lado2 && lado2 === lado3) {
+    console.log("El triángulo es equilátero.");
+}   
+else if (lado1 === lado2 || lado2 === lado3 || lado1 === lado3) {
+    console.log("El triángulo es isósceles.");
+}   
+else {
+    console.log("El triángulo es escaleno.");
+}   
+
