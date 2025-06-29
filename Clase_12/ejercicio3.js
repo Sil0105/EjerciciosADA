c095f9da2a49f156ea60c038b3379386409d0444
Sil0105 <pipita0105@gmail.com>
2025-06-28 21:01:22 -0300

    Agrego ejercicio3.js

---
+++ Clase_12/ejercicio3.js
@@ -0,0 +1,23 @@
+//Nuestra tarea es contar la cantidad de números impares que hay desde el número 0 hasta un número X (inclusive).
+//  Para esto, debes seguir los siguientes pasos:
+
+//Solicitar al usuario que ingrese un número X.
+//Inicializar una variable para contar los números impares.
+//Utilizar un ciclo for para recorrer los números desde 0 hasta X (inclusive).
+//Dentro del ciclo, verificar si el número es impar.
+//Si el número es impar, incrementar el contador de números impares.
+//Al finalizar el ciclo, imprimir en consola la cantidad de números impares encontrados.
+//Utiliza el operador módulo (%) para verificar si un número es impar.
+//Ejemplo de Ejecución: Si el usuario ingresa 10, el programa debe imprimir: “La cantidad de números impares desde 0 hasta 10 (inclusive) es: 5”
+
+const prompt = require('prompt-sync')();
+ let numeroX = prompt("Ingresa un número X: ");
+ 
+ let contadorImpares = 0;
+
+    for (let i = 0; i <= numeroX; i++) {
+        if (i % 2 !== 0) { 
+            contadorImpares++; 
+        }
+    }
+console.log(`La cantidad de números impares desde 0 hasta ${numeroX}  es: ${contadorImpares}`);
